6f303091e6b11562ae5653df02e5175665a17e37
zhongshijie <zhongshijie1995@outlook.com>
2024-07-27 00:46:09 +0800

    临时保存

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import time
+
 from data import data_loader
 from strategy import strategy_simple
 
 if __name__ == '__main__':
     code = 'AU2409'
-    s = data_loader.SinaLoader().get_realtime(code)
-    strategy_simple.MACD().add_row(s)
-    print(strategy_simple.MACD().get_df(s.get('代码')))
+    while True:
+        s = data_loader.SinaLoader().get_realtime(code)
+        strategy_simple.MACD().add_row(s)
+        time.sleep(1)
--- strategy/strategy_simple.py
+++ strategy/strategy_simple.py
@@ -1,3 +1,4 @@
+import copy
 from typing import Dict
 
 import pandas as pd
@@ -17,8 +18,23 @@ class MACD:
             self.df_dict[df_key] = pd.DataFrame([row])
         else:
             self.df_dict[df_key] = self.df_dict[df_key]._append(row, ignore_index=True)
+        MACD().get_golden_cross(self.df_dict[df_key])
 
     def get_df(self, df_key: str):
         return self.df_dict.get(df_key)
 
-    # short_period, long_period, signal_period
+    @staticmethod
+    def get_golden_cross(df, short_period=12, long_period=26, signal_period=9):
+        def calc_ema(x, col, span):
+            x['%s_EMA_%s' % (col, span)] = x[col].ewm(span=span, adjust=False).mean()
+
+        tmp = copy.deepcopy(df)
+        calc_ema(tmp, '最新价', short_period)
+        calc_ema(tmp, '最新价', long_period)
+        tmp['DIF'] = tmp['%s_EMA_%s' % ('最新价', short_period)] - tmp['%s_EMA_%s' % ('最新价', long_period)]
+        calc_ema(tmp, 'DIF', signal_period)
+        tmp['DEA'] = tmp['%s_EMA_%s' % ('DIF', signal_period)]
+        tmp['MACD'] = tmp['DIF'] - tmp['DEA']
+        tmp['golden_cross'] = (tmp['DIF'] < tmp['DEA'].shift(1)) & (tmp['DIF'].shift(-1) > tmp['DEA'].shift(-1))
+        if tmp.iloc[-1]['golden_cross'].tolist():
+            print(tmp.iloc[-1])

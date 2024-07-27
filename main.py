20a9f74e9abd9ba7ba84a7e9ed0dc31513d454d2
zhongshijie <zhongshijie1995@outlook.com>
2024-07-27 15:12:19 +0800

    临时保存

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from data import data_loader
+import pandas as pd
+
 from strategy import strategy_simple
 
 if __name__ == '__main__':
-    code = 'AU2409'
-    while True:
-        s = data_loader.SinaLoader().get_realtime(code)
-        strategy_simple.MACD().add_row(s)
+    # code = 'AU2409'
+    # while True:
+    #     s = data_loader.SinaLoader().get_realtime(code)
+    #     strategy_simple.MACD().add_row(s)
+
+    data = {
+        '最新价': [100, 101, 102, 103, 104, 105, 106, 107, 108, 107, 106, 105, 104, 103, 102, 101]  # 假设这里有一个价格下跌的趋势
+    }
+    df = pd.DataFrame(data)
+    buy_chance, sell_chance = strategy_simple.MACD().get_golden_death_cross(df)
+    print('做多')
+    print(buy_chance)
+    print('做空')
+    print(sell_chance)
--- strategy/strategy_simple.py
+++ strategy/strategy_simple.py
@@ -11,43 +11,50 @@ class MACD:
     def __init__(self):
         self.df_dict = {}
 
-    def add_row(self, row: Dict) -> None:
+    def add_row(self, row: Dict, max_history: int = 100) -> None:
         # 添加一条数据
         df_key = row.get('代码')
+        # 将该条数据转换为Pandas
+        tmp = pd.DataFrame([row])
+        # 将该条数据的Pandas插入到字典中
         if df_key not in self.df_dict:
-            self.df_dict[df_key] = pd.DataFrame([row])
+            self.df_dict[df_key] = tmp
         else:
-            if self.df_dict[df_key].iloc[-1]['时间'] != row.get('时间'):
-                self.df_dict[df_key] = pd.concat([self.df_dict[df_key], pd.DataFrame([row])])
+            if self.df_dict[df_key].iloc[-1]['时间'] < row.get('时间'):
+                self.df_dict[df_key] = pd.concat([self.df_dict[df_key], tmp])
+        # 遗忘300条以上的K线
+        if len(self.df_dict[df_key]) > max_history:
+            self.df_dict[df_key] = self.df_dict[df_key].iloc[-max_history:]
+        # 结束
+        return None
 
     def get_df(self, df_key: str):
         return self.df_dict.get(df_key)
 
     @staticmethod
-    def get_golden_cross(x, short_period=12, long_period=26, signal_period=9):
+    def get_golden_death_cross(x, short_period=12, long_period=26) -> [pd.DataFrame, pd.DataFrame]:
         def calc_ema(xx, col, span):
             xx['%s_EMA_%s' % (col, span)] = xx[col].ewm(span=span, adjust=False).mean()
 
+        # 拷贝数据集
         df = copy.deepcopy(x)
-        # 直接在原DataFrame上操作
+        # 计算
         calc_ema(df, '最新价', short_period)
         calc_ema(df, '最新价', long_period)
-        df['DIF'] = df['%s_EMA_%s' % ('最新价', short_period)] - df['%s_EMA_%s' % ('最新价', long_period)]
-        calc_ema(df, 'DIF', signal_period)
-        df['DEA'] = df['%s_EMA_%s' % ('DIF', signal_period)]
-        df['MACD'] = df['DIF'] - df['DEA']
 
         # 金叉逻辑
-        df['golden_cross'] = (df['%s_EMA_%s' % ('最新价', short_period)] > df['%s_EMA_%s' % ('最新价', long_period)]
-                              ) & (df['%s_EMA_%s' % ('最新价', short_period)].shift(1) <= df[
-            '%s_EMA_%s' % ('最新价', long_period)].shift(1))
+        df['golden_cross'] = (
+                (df['%s_EMA_%s' % ('最新价', short_period)] > df['%s_EMA_%s' % ('最新价', long_period)])
+                &
+                (df['%s_EMA_%s' % ('最新价', short_period)].shift(1) <= df['%s_EMA_%s' % ('最新价', long_period)].shift(
+                    1))
+        )
         # 死叉逻辑
-        df['death_cross'] = ((df['%s_EMA_%s' % ('最新价', short_period)] < df['%s_EMA_%s' % ('最新价', long_period)]) &
-                             (df['%s_EMA_%s' % ('最新价', short_period)].shift(1) >= df[
-                                 '%s_EMA_%s' % ('最新价', long_period)].shift(1)))
+        df['death_cross'] = (
+                (df['%s_EMA_%s' % ('最新价', short_period)] < df['%s_EMA_%s' % ('最新价', long_period)])
+                &
+                (df['%s_EMA_%s' % ('最新价', short_period)].shift(1) >= df['%s_EMA_%s' % ('最新价', long_period)].shift(
+                    1))
+        )
 
-        print([df.iloc[-1]['时间'], df.iloc[-1]['golden_cross'], df.iloc[-1]['death_cross']])
-
-        # # 如果最后一行是黄金交叉，则打印
-        # if df.iloc[-1]['golden_cross'] and df.iloc[-1]['death_cross']:
-        #     print(df.iloc[-1])
+        return df[df['golden_cross']], df[df['death_cross']]

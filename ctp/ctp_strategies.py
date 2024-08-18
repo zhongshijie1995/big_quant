a6f147ccdcba3a140adc37e84d800aab7f47d9af
钟世杰 <zhongshijie@192.168.0.100>
2024-08-18 22:44:58 +0800

    临时保存

--- ctp/ctp_strategies.py
+++ ctp/ctp_strategies.py
@@ -4,6 +4,8 @@ from ctpbee import CtpbeeApi
 from ctpbee.constant import ContractData, TickData
 from loguru import logger
 
+from ctp import ctp_tools
+
 
 class LogTick(CtpbeeApi):
     def __init__(self, contracts: List[str] = None):
@@ -17,4 +19,19 @@ class LogTick(CtpbeeApi):
             self.action.subscribe(contract.local_symbol)
 
     def on_tick(self, tick: TickData) -> None:
-        logger.info(f'{tick}')
+        logger.info(f'{ctp_tools.CtpTools().obj_to_dict(tick)}')
+
+
+class MacdCross(CtpbeeApi):
+    def __init__(self, contracts: List[str] = None):
+        super().__init__(self.__class__.__name__)
+        if contracts is None:
+            contracts = []
+        self.instrument_set = contracts
+
+    def on_contract(self, contract: ContractData):
+        if contract.local_symbol in self.instrument_set:
+            self.action.subscribe(contract.local_symbol)
+
+    def on_tick(self, tick: TickData):
+        pass

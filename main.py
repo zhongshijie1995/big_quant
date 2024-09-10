c7025e6a0e81f568c284a927305382b8ae9d879f
钟世杰 <zhongshijie@zhongshijiedeMacBook-Air.local>
2024-09-10 08:58:19 +0800

    重修移除UI

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import tkinter as tk
+from multiprocessing import Process
 
 from ctpbee import CtpBee, hickey
 
 from ctp import ctp_accounts, ctp_actions, ctp_contracts, ctp_strategies
 
 
-def ctpbee_start():
-    def create_ctpbee_app() -> CtpBee:
-        # 创建APP
-        app = CtpBee('big_quant', __name__, action_class=ctp_actions.CtpActions)
-        # 添加账户
-        app.config.from_mapping(ctp_accounts.CtpAccounts().acct_dict.get('simnow-01'))
-        # 添加策略
-        app.add_extension(ctp_strategies.MacdCross(ctp_contracts.CtpContracts().contracts))
-        return app
+def create_ctpbee_app() -> CtpBee:
+    # 创建APP
+    app = CtpBee('big_quant', __name__, action_class=ctp_actions.CtpActions)
+    # 添加账户
+    app.config.from_mapping(ctp_accounts.CtpAccounts().acct_dict.get('simnow-01'))
+    # 添加策略
+    app.add_extension(ctp_strategies.MacdCross(ctp_contracts.CtpContracts().contracts))
+    return app
 
+
+def ctpbee_start():
     # 7*24自动上下线
     hickey.start_all(create_ctpbee_app)
 
@@ -38,7 +40,7 @@ if __name__ == '__main__':
     # p_time = Process(target=time_start, args=())
     # p_time.start()
     # 开启ctpbee程序
-    # p_ctpbee = Process(target=ctpbee_start, args=())
-    # p_ctpbee.start()
+    p_ctpbee = Process(target=ctpbee_start, args=())
+    p_ctpbee.start()
     # GUI界面
-    gui_start()
+    # gui_start()

9903b21247b7c50479b8846f6ce39dd7461475ea
钟世杰 <zhongshijie@192.168.0.107>
2024-10-06 13:23:56 +0800

    临时保存

--- test.py
+++ test.py
@@ -1,24 +1,24 @@
 import matplotlib.pyplot as plt
 
-fig, ax = plt.subplots(figsize=(8, 8))
-colors = ['#ff9999', '#66b3ff']
+plt.rcParams["font.family"] = 'Arial Unicode MS'
+fig, ax = plt.subplots(figsize=(10, 6))
+labels = ['新多', '多持', '多平', '新空', '空持', '空平']
+colors = ['#ff9999', '#66b3ff', '#99ff99', '#ffcc99', '#ffa3a3', '#ffb3a3']
 wedges, texts, autotexts = ax.pie(
-    [20, 30],
-    labels=['多', '空'],
-    startangle=90,
-    autopct='%1.1f%%',
-    wedgeprops={'width': 0.5},
-    labeldistance=1.15,
-    colors=colors,
+    [3, 4, 5, 2, 3, 4],
+    labels=labels,
+    startangle=90, autopct='%.2f%%',
+    wedgeprops={'width': 0.3}, labeldistance=1.15,
+    colors=colors
 )
-
-plt.legend(
-    wedges,
-    texts,
-    loc='center left',
-    bbox_to_anchor=(1, 0.5),
+ax.set_title('多空对阵', size=20)
+ax.legend(
+    wedges, labels,
+    title='阵营',
+    loc="center left",
+    bbox_to_anchor=(1, 0, 0.5, 1),
+    fontsize=14
 )
-
-plt.setp(texts, fontsize=15)
-plt.setp(autotexts, fontsize=14)
+plt.setp(texts, size=15)
+plt.setp(autotexts, size=14)
 plt.show()

b61e160ad01c98cd555453e5325be1774eb9faa8
钟世杰 <zhongshijie@192.168.0.100>
2024-10-07 08:40:21 +0800

    临时保存

--- test.py
+++ test.py
@@ -1,28 +1,39 @@
+import tkinter as tk
+
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-plt.rcParams["font.family"] = 'Arial Unicode MS'
-fig, ax = plt.subplots(figsize=(10, 6))
-labels = ['新多', '多换', '空平', '多平', '空换', '新空']
-colors = ['#FF0000', '#FF6666', '#FF6600', '#66FFFF', '#66FF99', '#66FF00']
-wedges, texts, autotexts = ax.pie(
-    [3, 4, 5, 2, 3, 4],
-    labels=labels,
-    startangle=90,
-    autopct='%.2f%%',
-    wedgeprops={'width': 0.3},
-    labeldistance=1.15,
-    colors=colors
-)
-ax.set_title('多空对阵', size=20)
-ax.legend(
-    wedges,
-    labels,
-    title='阵营',
-    loc="center left",
-    bbox_to_anchor=(1, 0.5, 0.5, 0.5),
-    fontsize=14
-)
-plt.setp(texts, size=15)
-plt.setp(autotexts, size=14)
-plt.show()
 
+def buy_sell_power(
+        new_buy: int = 1,
+        change_buy: int = 1,
+        close_sell: int = 1,
+        close_buy: int = 1,
+        change_sell: int = 1,
+        new_sell: int = 1
+):
+    plt.rcParams["font.family"] = 'Arial Unicode MS'
+    fig, ax = plt.subplots()
+    labels = ['新多', '多换', '空平', '多平', '空换', '新空']
+    colors = ['#FF0000', '#FF6666', '#FF6600', '#66FFFF', '#66FF99', '#66FF00']
+    wedges, texts, autotexts = ax.pie(
+        [new_buy, change_buy, close_sell, close_buy, change_sell, new_sell],
+        labels=labels,
+        startangle=90,
+        autopct='%.2f%%',
+        wedgeprops={'width': 0.3},
+        labeldistance=1.15,
+        colors=colors
+    )
+    plt.setp(texts, size=12)
+    plt.setp(autotexts, size=12)
+    return fig
+
+
+root = tk.Tk()
+root.title("Tkinter with Matplotlib")
+fig = buy_sell_power()
+canvas = FigureCanvasTkAgg(fig, master=root)
+canvas.draw()
+canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+root.mainloop()
